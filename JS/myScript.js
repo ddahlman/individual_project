4062403f13e898616a0d84c618da1c56a8e6fa46
therealDaze <danieldahlman@outlook.com>
2016-11-07 17:01:46 +0100

    fixing sidebar

--- JS/myScript.js
+++ JS/myScript.js
@@ -3,6 +3,8 @@ var openThis = openNav.addEventListener('click', openNavbar);
 
 function openNavbar() {
     document.getElementById("mySidenav").style.width = "250px";
+    openNav.style.display = "none";
+    document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
 }
 
 var closeNav = document.getElementById("closeNav");
@@ -10,4 +12,6 @@ var closeThis = closeNav.addEventListener('click', closeNavbar);
 
 function closeNavbar() {
     document.getElementById("mySidenav").style.width = "0";
+    openNav.style.display = "block";
+    document.body.style.backgroundColor = "#fff";
 }

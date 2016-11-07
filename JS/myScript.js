e1e2dfa4b71b23d6fcb0dbcb77725eeffb3172f2
therealDaze <danieldahlman@outlook.com>
2016-11-07 21:52:39 +0100

    fixed sidebar and background color

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,17 +1,18 @@
 var openNav = document.getElementById("openNav");
 var openThis = openNav.addEventListener('click', openNavbar);
+var closeNav = document.getElementById("closeNav");
+var closeThis = closeNav.addEventListener('click', closeNavbar);
 
 function openNavbar() {
     document.getElementById("mySidenav").style.width = "250px";
     openNav.style.display = "none";
     document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+    document.getElementById("layer").style.backgroundColor = "rgba(0, 0, 0, 0.4)";
 }
-
-var closeNav = document.getElementById("closeNav");
-var closeThis = closeNav.addEventListener('click', closeNavbar);
 
 function closeNavbar() {
     document.getElementById("mySidenav").style.width = "0";
     openNav.style.display = "block";
-    document.body.style.backgroundColor = "#fff";
+    document.body.style.backgroundColor = "#fff";  
+    document.getElementById("layer").style.backgroundColor = ""; 
 }

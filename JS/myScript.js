a40498462dc105cc645bea2d202647fa362baefd
therealDaze <danieldahlman@outlook.com>
2016-11-09 19:15:28 +0100

    fixed layer id to layer class + cleaned about html

--- JS/myScript.js
+++ JS/myScript.js
@@ -2,17 +2,22 @@ var openNav = document.getElementById("openNav");
 var openThis = openNav.addEventListener('click', openNavbar);
 var closeNav = document.getElementById("closeNav");
 var closeThis = closeNav.addEventListener('click', closeNavbar);
+var layer = document.getElementsByClassName("layer");
 
 function openNavbar() {
     document.getElementById("mySidenav").style.width = "250px";
     openNav.style.display = "none";
     document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
-    document.getElementById("layer").style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+    for (var i = 0; i < layer.length; i++) {
+        layer[i].style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+    }
 }
 
 function closeNavbar() {
     document.getElementById("mySidenav").style.width = "0";
     openNav.style.display = "block";
-    document.body.style.backgroundColor = "#fff";  
-    document.getElementById("layer").style.backgroundColor = ""; 
+    document.body.style.backgroundColor = "#fff";
+    for (var i = 0; i < layer.length; i++) {
+        layer[i].style.backgroundColor = "";
+    }
 }

60968618c7e12cba3af2997c2a8ff37c61b6e568
therealDaze <danieldahlman@outlook.com>
2016-11-11 03:02:02 +0100

    finally fixed sidebar with css and js

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,22 +1,26 @@
 var openNav = document.getElementById("openNav");
-var openThis = openNav.addEventListener('click', openNavbar);
+openNav.addEventListener('click', openNavbar);
 var closeNav = document.getElementById("closeNav");
-var closeThis = closeNav.addEventListener('click', closeNavbar);
+closeNav.addEventListener('click', closeNavbar);
 var layer = document.getElementsByClassName("layer");
 
 function openNavbar() {
     document.getElementById("mySidenav").style.width = "250px";
     openNav.style.display = "none";
-    document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+
+    document.body.className = "openmenu";
+
     for (var i = 0; i < layer.length; i++) {
-        layer[i].style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+        layer[i].style.backgroundColor = "rgba(0, 0, 0, 0.8)";
     }
 }
 
 function closeNavbar() {
     document.getElementById("mySidenav").style.width = "0";
     openNav.style.display = "block";
-    document.body.style.backgroundColor = "#fff";
+
+    document.body.removeAttribute("class");
+
     for (var i = 0; i < layer.length; i++) {
         layer[i].style.backgroundColor = "";
     }

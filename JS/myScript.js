170f1aa42c7dd06382c28865dd39ddf1fd30fb59
therealDaze <danieldahlman@outlook.com>
2016-11-16 05:40:52 +0100

    added social media

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,27 +1,52 @@
-var openNav = document.getElementById("openNav");
-openNav.addEventListener('click', openNavbar);
-var closeNav = document.getElementById("closeNav");
-closeNav.addEventListener('click', closeNavbar);
-var layer = document.getElementsByClassName("layer");
+(function () {
 
-function openNavbar() {
-    document.getElementById("mySidenav").style.width = "250px";
-    openNav.style.display = "none";
 
-    document.body.className = "openmenu";
 
-    for (var i = 0; i < layer.length; i++) {
-        layer[i].style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+    var openNav = document.getElementById("openNav");
+    openNav.addEventListener('click', openNavbar);
+    var closeNav = document.getElementById("closeNav");
+    closeNav.addEventListener('click', closeNavbar);
+    var layer = document.getElementsByClassName("layer");
+
+    function openNavbar() {
+        document.getElementById("mySidenav").style.width = "250px";
+        openNav.style.display = "none";
+
+        document.body.className = "openmenu";
+
+        for (var i = 0; i < layer.length; i++) {
+            layer[i].style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+        }
     }
-}
 
-function closeNavbar() {
-    document.getElementById("mySidenav").style.width = "0";
-    openNav.style.display = "block";
+    function closeNavbar() {
+        document.getElementById("mySidenav").style.width = "0";
+        openNav.style.display = "block";
 
-    document.body.removeAttribute("class");
+        document.body.removeAttribute("class");
 
-    for (var i = 0; i < layer.length; i++) {
-        layer[i].style.backgroundColor = "";
+        for (var i = 0; i < layer.length; i++) {
+            layer[i].style.backgroundColor = "";
+        }
     }
-}
+
+})();
+
+/*end of sidebar=============================================*/
+
+(function () {
+    $('.share').ShareLink({
+        title: 'Daniel Dahlman',
+        text: 'Visit my website!',
+        image: 'http://www.jqueryscript.net/images/logo.png',
+        url: 'http://www.danieldahlman.se',
+        class_prefix: 's_'
+    });
+
+    $('.counter').ShareCounter({
+        url: 'http://www.danieldahlman.se',
+        class_prefix: 'c_',
+        display_counter_from: 0,
+        increment: false
+    });
+})();

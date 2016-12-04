3fca12b1e21d56890fabeba14f07f36f286531da
therealDaze <danieldahlman@outlook.com>
2016-12-04 17:49:53 +0100

    added folder for includes and included footer and header

--- résumé.php
+++ résumé.php
@@ -1,192 +1,199 @@
-<div class="flex-col-img2">
-  <img src="img/daniel in progress copy2copy.jpg" alt="daniel dahlman">
-  <div class="h1-header-about">
-    <h1 class="h1-flex-about"> C V </h1>
+<?php include('./includes/header.php');
+?>
+
+  <div class="flex-col-img2">
+    <img src="img/daniel in progress copy2copy.jpg" alt="daniel dahlman">
+    <div class="h1-header-about">
+      <h1 class="h1-flex-about"> C V </h1>
+    </div>
+    <div class="like-block">
+      <button class="like-button share s_twitter"><span class="nr2 fa fa-twitter-square"></span></button>
+      <button class="like-button share s_facebook"><span class="nr2 fa fa-facebook-square"></span></button>
+      <button class="like-button share s_plus"><span class="nr2 fa fa-google-plus-official"></span></button>
+      <button class="like-button share s_linkedin"><span class="nr2 fa fa-linkedin-square"></span></button>
+    </div>
   </div>
-  <div class="like-block">
-    <button class="like-button share s_twitter"><span class="nr2 fa fa-twitter-square"></span></button>
-    <button class="like-button share s_facebook"><span class="nr2 fa fa-facebook-square"></span></button>
-    <button class="like-button share s_plus"><span class="nr2 fa fa-google-plus-official"></span></button>
-    <button class="like-button share s_linkedin"><span class="nr2 fa fa-linkedin-square"></span></button>
+  <div class="flex-row">
+    <div class="flex-col">
+      <p>
+        Jag studerar på den framgångsrika utbildningen Front End Developer på KYH i Stockholm som specialiserar sig inom webbutveckling och webbsidor och hur man kan få dom så användarvänliga och snabbladdade som möjligt. Förutom de grundläggande programmeringsspråken
+        som HTML, CSS och Javascript så vidareutvecklar vi oss i PHP och andra tekniker för att kunna utföra en webbuplevelse som passar alla behov.
+      </p>
+    </div>
   </div>
-</div>
-<div class="flex-row">
-  <div class="flex-col">
-    <p>
-      Jag studerar på den framgångsrika utbildningen Front End Developer på KYH i Stockholm som specialiserar sig inom webbutveckling och webbsidor och hur man kan få dom så användarvänliga och snabbladdade som möjligt. Förutom de grundläggande programmeringsspråken
-      som HTML, CSS och Javascript så vidareutvecklar vi oss i PHP och andra tekniker för att kunna utföra en webbuplevelse som passar alla behov.
-    </p>
-  </div>
-</div>
-<div class="flex-row-2">
-  <div class="flex-col-card grey-pants expand">
-    <h1 class="h1-about"> Tekniska färdigheter</h1>
-    <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
-    <ul class="myInfo">
-      <li>C# Grund</li>
-      <li>Databaser SQL</li>
-      <li>Arbeta i Agila projekt</li>
-      <li>Objektorienterad programmering i C#</li>
-      <li>ASP.Net</li>
-      <li>Web-applikationer</li>
-      <li>HTML 5</li>
-      <li>CSS 3</li>
-      <li>Bootstrap frameworks</li>
-      <li>Jquery</li>
-      <li>Javascript</li>
-      <li>Visual Studio</li>
-      <li>Team Foundation Server</li>
-    </ul>
-  </div>
-  <div class="flex-col-card grey-pants expand">
-    <h1 class="h1-about">Personliga färdigheter</h1>
-    <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
-    <ul class="myInfo">
-      <li>Positiv och social</li>
-      <li>Talar och skriver flytande engelska</li>
-      <li>Har pedagogiska färdigheter</li>
-      <li>Flexibel</li>
-      <li>Är kreativ och har inga problem med att ta egna initiativ</li>
-    </ul>
-  </div>
-  <div class="flex-col-card grey-pants expand">
-    <h1 class="h1-about">Körkort</h1>
-    <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
-    <ul class="myInfo">
-      <li>Ja, B-körkort</li>
-    </ul>
-  </div>
-</div>
-<h1 class="h1-col">- P r o j e k t -</h1>
-<div class="flex-col-card project expand">
-  <h2>Ärendehanteringssystem för bilar</h2>
-  <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
-  <hr class="hr" />
-  <div class="myInfo">
-    <p>
-      Det här var ett ASP.NET projekt, som gjordes i MVC C#, som jag var involverad i. Jag skapade t.ex. en service som hanterade mail genom SMTP där användaren klickade på en knapp och ett mail skickades iväg för att begära en granskning ifrån olika granskare.
-      Jag skapade också en uppladdnings-tjänst där användaren skulle kunna klicka på en knapp, eller på en placeholder, för att leta efter en bild. Bilden skulle sen kunna förhandsgranskas av användaren innan det bestämdes för att ladda upp den.
-    </p>
-    <div class="flex-footer grey-pants">
-      <h1>tekniker</h1>
-      <ul>
-        <li>C#</li>
+  <div class="flex-row-2">
+    <div class="flex-col-card grey-pants expand">
+      <h1 class="h1-about"> Tekniska färdigheter</h1>
+      <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
+      <ul class="myInfo">
+        <li>C# Grund</li>
+        <li>Databaser SQL</li>
+        <li>Arbeta i Agila projekt</li>
+        <li>Objektorienterad programmering i C#</li>
+        <li>ASP.Net</li>
+        <li>Web-applikationer</li>
+        <li>HTML 5</li>
+        <li>CSS 3</li>
+        <li>Bootstrap frameworks</li>
         <li>Jquery</li>
         <li>Javascript</li>
-        <li>html</li>
-        <li>css</li>
-        <li>Bootstrap</li>
+        <li>Visual Studio</li>
+        <li>Team Foundation Server</li>
+      </ul>
+    </div>
+    <div class="flex-col-card grey-pants expand">
+      <h1 class="h1-about">Personliga färdigheter</h1>
+      <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
+      <ul class="myInfo">
+        <li>Positiv och social</li>
+        <li>Talar och skriver flytande engelska</li>
+        <li>Har pedagogiska färdigheter</li>
+        <li>Flexibel</li>
+        <li>Är kreativ och har inga problem med att ta egna initiativ</li>
+      </ul>
+    </div>
+    <div class="flex-col-card grey-pants expand">
+      <h1 class="h1-about">Körkort</h1>
+      <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
+      <ul class="myInfo">
+        <li>Ja, B-körkort</li>
       </ul>
     </div>
   </div>
-</div>
-<div class="flex-col-card project expand">
-  <h2>Bygga upp webbsida för bilföretag</h2>
-  <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
-  <hr class="hr" />
-  <div class="myInfo">
-    <p>
-      Jag fick i uppdrag att skapa en dubblett av en webbsida som hade skapats i Joomla, fast nu med ren html och css. Responsiviteten skulle bara utgå från ren css och jag använde mig då av en wrapper som hade css-attributen "display: flex;, justify-content:
-      center; align-items: center;" och självklart några media-querys för att fixa det. Det kluriga var att jag skulle skapa en border utav stjärnor som självklart inte fanns på Google. Jag fick lov att jobba utanför ramarna och skapa en egen ram utav
-      stjärnor i Adobe Illustator. Ramen använde jag i css-attributet "border-image;" som jag sedan kunde justera till den storleken jag ville ha.
-    </p>
-    <div class="flex-footer grey-pants">
-      <h1>tekniker</h1>
-      <ul>
-        <li>html</li>
-        <li>css</li>
-        <li>Adobe Illustrator</li>
-      </ul>
+  <h1 class="h1-col">- P r o j e k t -</h1>
+  <div class="flex-col-card project expand">
+    <h2>Ärendehanteringssystem för bilar</h2>
+    <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
+    <hr class="hr" />
+    <div class="myInfo">
+      <p>
+        Det här var ett ASP.NET projekt, som gjordes i MVC C#, som jag var involverad i. Jag skapade t.ex. en service som hanterade mail genom SMTP där användaren klickade på en knapp och ett mail skickades iväg för att begära en granskning ifrån olika granskare.
+        Jag skapade också en uppladdnings-tjänst där användaren skulle kunna klicka på en knapp, eller på en placeholder, för att leta efter en bild. Bilden skulle sen kunna förhandsgranskas av användaren innan det bestämdes för att ladda upp den.
+      </p>
+      <div class="flex-footer grey-pants">
+        <h1>tekniker</h1>
+        <ul>
+          <li>C#</li>
+          <li>Jquery</li>
+          <li>Javascript</li>
+          <li>html</li>
+          <li>css</li>
+          <li>Bootstrap</li>
+        </ul>
+      </div>
     </div>
   </div>
-</div>
-<div class="flex-col-card project expand">
-  <h2>Affärssida som integrerar med en annan apps API</h2>
-  <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
-  <hr class="hr" />
-  <div class="myInfo">
-    <p>
-      Under det här projektet fick jag skapa en "storefront"-sida som integrerade med en e-butik som använde sig utav rest API. Då ingick det att skapa kolumner med klickbara placeholders med underliggande information. När användaren klickade på en placeholder
-      skulle en modal visas med en carousel som hade fler placeholders, ungefär som en vanlig webbshop. Sedan skapade jag en "shopping cart"-knapp som öppnade ännu en modal där användaren kunde se sina tillagda varor och därefter kunde klicka på en knapp
-      och gå vidare till ett utcheckningsformulär (fortfarande i modal-fönstret).
-    </p>
-    <div class="flex-footer grey-pants">
-      <h1>tekniker</h1>
-      <ul>
-        <li>Jquery</li>
-        <li>html</li>
-        <li>css</li>
-        <li>Bootstrap</li>
-        <li>javascript</li>
-      </ul>
+  <div class="flex-col-card project expand">
+    <h2>Bygga upp webbsida för bilföretag</h2>
+    <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
+    <hr class="hr" />
+    <div class="myInfo">
+      <p>
+        Jag fick i uppdrag att skapa en dubblett av en webbsida som hade skapats i Joomla, fast nu med ren html och css. Responsiviteten skulle bara utgå från ren css och jag använde mig då av en wrapper som hade css-attributen "display: flex;, justify-content:
+        center; align-items: center;" och självklart några media-querys för att fixa det. Det kluriga var att jag skulle skapa en border utav stjärnor som självklart inte fanns på Google. Jag fick lov att jobba utanför ramarna och skapa en egen ram utav
+        stjärnor i Adobe Illustator. Ramen använde jag i css-attributet "border-image;" som jag sedan kunde justera till den storleken jag ville ha.
+      </p>
+      <div class="flex-footer grey-pants">
+        <h1>tekniker</h1>
+        <ul>
+          <li>html</li>
+          <li>css</li>
+          <li>Adobe Illustrator</li>
+        </ul>
+      </div>
     </div>
   </div>
-</div>
-<div class="flex-col expand">
-  <h1 class="h1-col">Anställningar och erfarenheter</h1>
-  <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
-  <hr class="hr" />
-  <ul class="myInfo">
-    <li class="li-education">12 veckor Praktik hos konsultföretaget Walter IT Systems AB- AGILE MANAGEMENT AND DEVELOPMENT</li>
-    <li class="li-education">2016</li>
-    <li class="li-education">
-      <br />ledarerfarenhet inom dansverksamhet både i Sverige och utomlands</li>
-    <li class="li-education">1999 - 2016</li>
-    <li class="li-education">
-      <br />SVT Melodifestivalen, Deltävling 4, Örebro, Koreograf och Dansare för öppningsnummer</li>
-    <li class="li-education"> 2015</li>
-    <li class="li-education">
-      <br />Produktionen ”SIRI”, Stockholm, Complete Dance Crew på Dansens Hus och Riks Turné Koreograf och Dansare</li>
-    <li class="li-education"> 2013 – 2015</li>
-    <li class="li-education">
-      <br />After Dark Jubileumsshow, Sthlm, Malmö, Gbg, Danskapten, Koreograf och Dansare</li>
-    <li class="li-education"> 2012 – 2013</li>
-    <li class="li-education">
-      <br />Väskan AB, VästeråsButiksbiträde, Sommaranställning</li>
-    <li class="li-education"> 2012</li>
-    <li class="li-education">
-      <br />”The Last Bounce”, Globen StockholmDansare</li>
-    <li class="li-education"> 2010</li>
-    <li class="li-education">
-      <br />Musikalen ”West Side Story”, Norrlandsoperan, Umeå, Sångare och Dansare</li>
-    <li class="li-education"> 2010</li>
-    <li class="li-education">
-      <br />Complete Dance Crew Grundare, Koreograf och Dansare</li>
-    <li class="li-education"> 2008 – 2015</li>
-  </ul>
-</div>
-<div class="flex-col-card grey-pants expand">
-  <h1 class="h1-index-yellow">Meritförteckning inom de tekniska färdigheterna</h1>
-  <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
-  <ul class="myInfo">
-    <li>OOP C#<span>VG</span></li>
-    <li>C# Grund<span>VG</span></li>
-    <li>Databaser SQL<span>G</span></li>
-    <li>Arbeta i Agila Projekt<span>G</span></li>
-    <li>Asp.Net<span>VG</span></li>
-    <li>Webbaplikationer<span>VG</span></li>
-  </ul>
-</div>
-<div class="flex-col expand">
-  <h1 class="h1-col">Utbildningar</h1>
-  <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
-  <hr class="hr" />
-  <ul class="myInfo">
-    <li class="li-education">
-      Teknikhögskolan, Stockholm Applikationsutveckling inom .NET
-    </li>
-    <li class="li-education">2015 – 2017</li>
-    <li class="li-education">
-      <br />Hermods, Västerås Komvux C#, Ma2bc, Sv2
-    </li>
-    <li class="li-education">2015 – 2015</li>
-    <li class="li-education">
-      <br />Balettakademien, Stockholm Yrkesutbildning
-    </li>
-    <li class="li-education">2004 – 2006</li>
-    <li class="li-education">
-      <br />Carlforsska Gymnasiet, Västerås Gymnasial utbildning 3-årig
-    </li>
-    <li class="li-education">2001 – 2004</li>
-  </ul>
-</div>
+  <div class="flex-col-card project expand">
+    <h2>Affärssida som integrerar med en annan apps API</h2>
+    <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
+    <hr class="hr" />
+    <div class="myInfo">
+      <p>
+        Under det här projektet fick jag skapa en "storefront"-sida som integrerade med en e-butik som använde sig utav rest API. Då ingick det att skapa kolumner med klickbara placeholders med underliggande information. När användaren klickade på en placeholder
+        skulle en modal visas med en carousel som hade fler placeholders, ungefär som en vanlig webbshop. Sedan skapade jag en "shopping cart"-knapp som öppnade ännu en modal där användaren kunde se sina tillagda varor och därefter kunde klicka på en
+        knapp och gå vidare till ett utcheckningsformulär (fortfarande i modal-fönstret).
+      </p>
+      <div class="flex-footer grey-pants">
+        <h1>tekniker</h1>
+        <ul>
+          <li>Jquery</li>
+          <li>html</li>
+          <li>css</li>
+          <li>Bootstrap</li>
+          <li>javascript</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="flex-col expand">
+    <h1 class="h1-col">Anställningar och erfarenheter</h1>
+    <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
+    <hr class="hr" />
+    <ul class="myInfo">
+      <li class="li-education">12 veckor Praktik hos konsultföretaget Walter IT Systems AB- AGILE MANAGEMENT AND DEVELOPMENT</li>
+      <li class="li-education">2016</li>
+      <li class="li-education">
+        <br />ledarerfarenhet inom dansverksamhet både i Sverige och utomlands</li>
+      <li class="li-education">1999 - 2016</li>
+      <li class="li-education">
+        <br />SVT Melodifestivalen, Deltävling 4, Örebro, Koreograf och Dansare för öppningsnummer</li>
+      <li class="li-education"> 2015</li>
+      <li class="li-education">
+        <br />Produktionen ”SIRI”, Stockholm, Complete Dance Crew på Dansens Hus och Riks Turné Koreograf och Dansare</li>
+      <li class="li-education"> 2013 – 2015</li>
+      <li class="li-education">
+        <br />After Dark Jubileumsshow, Sthlm, Malmö, Gbg, Danskapten, Koreograf och Dansare</li>
+      <li class="li-education"> 2012 – 2013</li>
+      <li class="li-education">
+        <br />Väskan AB, VästeråsButiksbiträde, Sommaranställning</li>
+      <li class="li-education"> 2012</li>
+      <li class="li-education">
+        <br />”The Last Bounce”, Globen StockholmDansare</li>
+      <li class="li-education"> 2010</li>
+      <li class="li-education">
+        <br />Musikalen ”West Side Story”, Norrlandsoperan, Umeå, Sångare och Dansare</li>
+      <li class="li-education"> 2010</li>
+      <li class="li-education">
+        <br />Complete Dance Crew Grundare, Koreograf och Dansare</li>
+      <li class="li-education"> 2008 – 2015</li>
+    </ul>
+  </div>
+  <div class="flex-col-card grey-pants expand">
+    <h1 class="h1-index-yellow">Meritförteckning inom de tekniska färdigheterna</h1>
+    <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
+    <ul class="myInfo">
+      <li>OOP C#<span>VG</span></li>
+      <li>C# Grund<span>VG</span></li>
+      <li>Databaser SQL<span>G</span></li>
+      <li>Arbeta i Agila Projekt<span>G</span></li>
+      <li>Asp.Net<span>VG</span></li>
+      <li>Webbaplikationer<span>VG</span></li>
+    </ul>
+  </div>
+  <div class="flex-col expand">
+    <h1 class="h1-col">Utbildningar</h1>
+    <a href="#" data-toggle="expand">mer info<i class="fa fa-plus-circle"></i></a>
+    <hr class="hr" />
+    <ul class="myInfo">
+      <li class="li-education">
+        Teknikhögskolan, Stockholm Applikationsutveckling inom .NET
+      </li>
+      <li class="li-education">2015 – 2017</li>
+      <li class="li-education">
+        <br />Hermods, Västerås Komvux C#, Ma2bc, Sv2
+      </li>
+      <li class="li-education">2015 – 2015</li>
+      <li class="li-education">
+        <br />Balettakademien, Stockholm Yrkesutbildning
+      </li>
+      <li class="li-education">2004 – 2006</li>
+      <li class="li-education">
+        <br />Carlforsska Gymnasiet, Västerås Gymnasial utbildning 3-årig
+      </li>
+      <li class="li-education">2001 – 2004</li>
+    </ul>
+  </div>
+
+  <?php
+include('./includes/footer.php');
+?>

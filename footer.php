e868e96145dd872f6428aca8bff868181886e573
therealDaze <danieldahlman@outlook.com>
2016-12-04 17:33:40 +0100

    changed all html pages to php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer>
+  <ul>
+    <li>Daniel Dahlman</li>
+    <li><a href="mailto:danieldahlman@outlook.com">danieldahlman@outlook.com</a></li>
+    <li>
+      <a href="https://se.linkedin.com/in/daniel-dahlman-6bb9438a"><span class="fa fa-linkedin-square"> linkedin</span></a>
+    </li>
+    <li>0739-18 22 16</li>
+  </ul>
+</footer>
+<script src="JS/jquery-3.1.1.min.js"></script>
+<script src="JS/SocialShare.js"></script>
+<script src="JS/myScript.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="sv-SE">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Daniel Dahlman</title>
+  <link rel="stylesheet" href="font/css/font-awesome.min.css">
+  <link rel="stylesheet" href="CSS/myStyle.css">
+</head>
+
+<body>
+  <span class="hamburger fa fa-navicon" id="openNav"></span>
+  <div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" id="closeNav" class="closebtn fa fa-close"></a>
+    <a class="page-links" href="index.php">HEM <hr class="hr"></a>
+    <a class="page-links" href="résumé.php">CV <hr class="hr"></a>
+    <a class="page-links" href="about.php">OM MIG <hr class="hr"></a>
+  </div>
+  <div class="layer"></div>

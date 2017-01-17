9e7006ec21245370ad3330976b7cc610fa2a6f9b
therealDaze <danieldahlman@outlook.com>
2017-01-17 21:34:00 +0100

    done with session.php

--- hemlig_admin/session.php
+++ hemlig_admin/session.php
@@ -1,4 +1,14 @@
 <?php
 include('config.php');
+session_start();
+$connection = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbdatabas);
+$user_check = $_SESSION['login_user'];
+$session_sql = mysqli_query($connection, "SELECT username FROM login WHERE username = '$user_check'");
+$row = mysqli_fetch_assoc($session_sql);
+$login_session = $row['username'];
 
+if(!isset($login_session)) {
+    mysqli_close($connection);
+    header('Location: index.php');
+}
 ?>

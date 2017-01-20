8437b3061d76f2d783d4f3849fa9c29b2a039a71
therealDaze <danieldahlman@outlook.com>
2017-01-20 04:39:03 +0100

    fixed tables in sql and fixed edit in cv first-text

--- hemlig_admin/admin_footer.php
+++ hemlig_admin/admin_footer.php
@@ -2,7 +2,7 @@
 
 </footer>
 <script src="../JS/jquery-3.1.1.min.js"></script>
-<script src="../JS/myScript.js"></script>
+<script src="../JS/myScript.js" charset="utf-8"></script>
 </body>
 
 </html>
--- hemlig_admin/cv.php
+++ hemlig_admin/cv.php
@@ -2,5 +2,29 @@
 
 $connection = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbdatabase);
 
+$query1 = mysqli_query($connection, "SELECT * FROM cv_firsttext WHERE id=1");
 
+$cv_first_text = mysqli_fetch_assoc($query1);
+
+$first_text = $cv_first_text["first_text"];
+
+if(isset($_POST['save'])) {
+    $first_text = $_POST["first-text"];
+    
+    mysqli_query($connection, "UPDATE cv_firsttext
+    SET first_text='$first_text'
+    WHERE id=1");
+    
+    $success = 'CV är uppdatterat!';
+}
 ?>
+  <div class="flex-container">
+    <div class="flex-center-center">
+      <div class="grey-bg">
+        <form method="post">
+          <textarea name="first-text" id="first-text" cols="30" rows="10" placeholder="<?php echo $first_text; ?>"></textarea>
+          <input type="submit" name="save" value="spara">
+        </form>
+      </div>
+    </div>
+  </div>

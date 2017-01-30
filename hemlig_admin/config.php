bc576ae0765d553db6c6b6048e2155b5cc2754f6
therealDaze <danieldahlman@outlook.com>
2017-01-30 18:33:49 +0100

    added the config.php file again

---
+++ hemlig_admin/config.php
@@ -0,0 +1,10 @@
+<?php
+$dbusername = "219307_ot65650";
+$dbhost = "danieldahlman-219307.mysql.binero.se";
+$dbpassword = "Leinad0243";
+$dbdatabase = "219307-danieldahlman";
+?>
+  <!--$dbusername = "root";
+$dbhost = "localhost";
+$dbpassword = "";
+$dbdatabas = "test";-->
